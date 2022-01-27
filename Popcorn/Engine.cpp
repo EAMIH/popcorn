7b6c94549fef4965aa2f9839a830319c316a5c51
Evgeniy Miheev <emiheev55555@gmail.com>
2022-01-27 11:12:09 +1000

    Prototype 6

--- Popcorn/Engine.cpp
+++ Popcorn/Engine.cpp
@@ -15,8 +15,12 @@ enum EBrick_Type
 	EBT_Blue
 };
 
-HPEN Highlight_Pen, Letter_Pen, Brick_Red_Pen, Brick_Blue_Pen, Platform_Circle_Pen, Platform_Inner_Pen;
-HBRUSH Brick_Red_Brush, Brick_Blue_Brush, Platform_Circle_Brush, Platform_Inner_Brush;
+HWND Hwnd;
+HPEN Highlight_Pen, Letter_Pen, BG_Pen, Brick_Red_Pen, Brick_Blue_Pen, Platform_Circle_Pen, Platform_Inner_Pen;
+HBRUSH BG_Brush, Brick_Red_Brush, Brick_Blue_Brush, Platform_Circle_Brush, Platform_Inner_Brush;
+RECT Platform_Rect, Prev_Platform_Rect;
+RECT Level_Rect;
+
 
 const int Global_Scale = 3;
 const int Brick_Width = 15;
@@ -26,11 +30,19 @@ const int Cell_Width = 16;
 const int Cell_Height = 8;
 const int Level_X_Offset = 8;
 const int Level_Y_Offset = 6;
+const int Level_Width = 12;
+const int Level_Height = 14;
 const int Circle_Size = 7;
+const int Platform_Y_Pos = 185;
+int Platform_Height = 7;
 
+int Platform_X_Pos = 0;
 int Inner_Width = 21;
+int Platform_X_Step = Global_Scale * 2;
+int Platform_Width = 28;
 
-char Level_01[14][12] =
+
+char Level_01[Level_Height][Level_Width] =
 {
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -55,15 +67,38 @@ void Create_Pen_Brush(unsigned char r, unsigned char g, unsigned char b, HPEN &p
    brush = CreateSolidBrush(RGB(r, g, b));
 }
 //------------------------------------------------------------------------------------------------------------
-void Init()
+void Redraw_Platform()
+{// Отрисовка экрана игры
+   Prev_Platform_Rect = Platform_Rect;
+
+   Platform_Rect.left = (Level_X_Offset + Platform_X_Pos) * Global_Scale;
+   Platform_Rect.top = Platform_Y_Pos * Global_Scale;
+   Platform_Rect.right = Platform_Rect.left + Platform_Width * Global_Scale;
+   Platform_Rect.bottom = Platform_Rect.top + Platform_Height * Global_Scale;
+
+   Level_Rect.left = Cell_Width * Global_Scale;
+   Level_Rect.top = Cell_Height * Global_Scale;
+   Level_Rect.right = Level_Rect.left + Cell_Width * Level_Width * Global_Scale;
+   Level_Rect.bottom = Level_Rect.top + Cell_Height * Level_Height * Global_Scale;
+
+   InvalidateRect(Hwnd, &Prev_Platform_Rect, FALSE);
+   InvalidateRect(Hwnd, &Platform_Rect, FALSE);
+}
+//------------------------------------------------------------------------------------------------------------
+
+void Init_Engine(HWND hwnd)
 {// Настройка игры при старте
+   Hwnd = hwnd;
    Highlight_Pen = CreatePen(PS_SOLID, 0, RGB(255, 255, 255));
    Letter_Pen = CreatePen(PS_SOLID, Global_Scale, RGB(255, 255, 255));
-
+      
+   Create_Pen_Brush(15, 63, 31, BG_Pen, BG_Brush);
    Create_Pen_Brush(255, 85, 85, Brick_Red_Pen, Brick_Red_Brush);
    Create_Pen_Brush(85, 255, 255, Brick_Blue_Pen, Brick_Blue_Brush);
    Create_Pen_Brush(151, 0, 0, Platform_Circle_Pen, Platform_Circle_Brush);
    Create_Pen_Brush(0, 128, 192, Platform_Inner_Pen, Platform_Inner_Brush);
+
+   Redraw_Platform();
 }
 //------------------------------------------------------------------------------------------------------------
 void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type)
@@ -246,6 +281,11 @@ void Draw_Brick_Letter(HDC hdc, int x, int y, EBrick_Type brick_type, ELetter_ty
 void Draw_Platform(HDC hdc, int x, int y)
 {// Отрисовка экрана игры
 
+   SelectObject(hdc, BG_Pen);
+   SelectObject(hdc, BG_Brush);
+
+   Rectangle(hdc, Prev_Platform_Rect.left, Prev_Platform_Rect.top, Prev_Platform_Rect.right, Prev_Platform_Rect.bottom);
+
    SelectObject(hdc, Platform_Circle_Pen);
    SelectObject(hdc, Platform_Circle_Brush);
 
@@ -262,15 +302,42 @@ void Draw_Platform(HDC hdc, int x, int y)
   	RoundRect(hdc, (x + 4) * Global_Scale, (y + 1) * Global_Scale, (x + 4 + Inner_Width - 1) * Global_Scale, (y + 1 + 5) * Global_Scale, 3 * Global_Scale, 3 * Global_Scale);
 }
 //------------------------------------------------------------------------------------------------------------
-void Draw_Frame(HDC hdc)
+void Draw_Frame(HDC hdc, RECT &paint_area)
 {// Отрисовка экрана игры
-   //Draw_Level(hdc);
-   //Draw_Platform(hdc, 50, 100);
-   int i;
-   for (i = 0; i < 16; i++)
-   {
-      Draw_Brick_Letter(hdc, 20 + i * Cell_Width * Global_Scale, 100, EBT_Blue, ELT_O, i);
-      Draw_Brick_Letter(hdc, 20 + i * Cell_Width * Global_Scale, 130, EBT_Red, ELT_O,  i);
-   }
+   RECT intersektion_rect;
+
+   if(IntersectRect(&intersektion_rect, &paint_area, &Level_Rect))
+      Draw_Level(hdc);
+   
+   if(IntersectRect(&intersektion_rect, &paint_area, &Platform_Rect))
+      Draw_Platform(hdc, Level_X_Offset + Platform_X_Pos, Platform_Y_Pos);
+    
+   //int i;
+   //for (i = 0; i < 16; i++)
+   //{
+   //   Draw_Brick_Letter(hdc, 20 + i * Cell_Width * Global_Scale, 100, EBT_Blue, ELT_O, i);
+   //   Draw_Brick_Letter(hdc, 20 + i * Cell_Width * Global_Scale, 130, EBT_Red, ELT_O,  i);
+   //}
 }
 //------------------------------------------------------------------------------------------------------------
+int On_Key_Down(EKey_Type key_type)
+{
+   switch (key_type)
+   {
+   case EKT_Left:
+      Platform_X_Pos -= Platform_X_Step;
+      Redraw_Platform();
+      break;
+
+   case EKT_Right:
+      Platform_X_Pos += Platform_X_Step;
+      Redraw_Platform();
+      break;
+
+   case EKT_Space:
+
+      break;
+   }
+
+   return 0;
+}
