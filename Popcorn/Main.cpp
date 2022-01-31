14fcd2c03bf8ea0e8fd1731e84ff9011a28924b6
Evgeniy Miheev <emiheev55555@gmail.com>
2022-01-31 20:13:56 +1000

    Prototype 9

--- Popcorn/Main.cpp
+++ Popcorn/Main.cpp
@@ -184,10 +184,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		 }
 		 break;
 
-    case WM_TIMER:
-       if (wParam == Timer_Id)
-          return On_Timer();
-          break;
+
+	 case WM_TIMER:
+		 if (wParam == Timer_ID)
+			 return On_Timer();
+		 break;
 
 
     default:
