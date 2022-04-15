3c783d2d4eac211fba782374384b70f047423bc8
Evgeniy Miheev <emiheev55555@gmail.com>
2022-04-15 19:15:35 +1000

    Part 60 Expanding platform, Stage 2

--- Popcorn/Platform.h
+++ Popcorn/Platform.h
@@ -7,6 +7,8 @@
 //------------------------------------------------------------------------------------------------------------
 enum class EPlatform_State: unsigned char
 {
+	Unknown,
+
 	Regular,
 	Meltdown,
 	Rolling,
@@ -73,6 +75,9 @@ public:
 	operator EPlatform_State() const;
 	void operator = (EPlatform_State new_state);
 
+	void Set_Next_State(EPlatform_State next_state);
+	EPlatform_State Get_Next_State();
+
 	EPlatform_Substate_Regular Regular;
 	EPlatform_Substate_Meltdown Meltdown;
 	EPlatform_Substate_Rolling Rolling;
@@ -83,7 +88,7 @@ public:
 
 private:
 	EPlatform_State Current_State;
-
+	EPlatform_State Next_State;  // В это состояние переходим из AsPlatform::Set_State(EPlatform_Substate_Regular new_regular_state)
 };
 //------------------------------------------------------------------------------------------------------------
 class AsPlatform: public AHit_Checker, public AMover, public AGraphics_Object
@@ -109,18 +114,18 @@ public:
 	void Set_State(EPlatform_State new_state);
 	void Set_State(EPlatform_Substate_Regular new_regular_state);
 	bool Has_State(EPlatform_Substate_Regular regular_state);
-	void Redraw_Platform(bool update_rect = true);
+	void Redraw_Platform();
 	void Move(bool to_left, bool key_down);
 	void On_Space_Key(bool key_down);
 	bool Hit_By(AFalling_Letter *falling_letter);
 	double Get_Middle_Pos();
-	int Width;
 	double X_Pos;
 
 private:
 	void Act_For_Meltdown_State();
 	void Act_For_Rolling_State();
 	void Act_For_Glue_State();
+	void Act_For_Expanding_State();
 	void Draw_Circle_Highlight(HDC hdc, int x, int y);
 	void Draw_Normal_State(HDC hdc, RECT &paint_area);
 	void Draw_Meltdown_State(HDC hdc, RECT &paint_area);
@@ -134,15 +139,17 @@ private:
 	bool Reflect_On_Circle(double next_x_pos, double next_y_pos, double platform_ball_x_offset, ABall *ball);
 	bool Get_Platform_Image_Stroke_Color(int x, int y, const AColor **color, int &stroke_len);
 	void Get_Normal_Platform_Image(HDC hdc);
-
+	double Get_Current_Width();
+	bool Correct_Platform_Pos();
+	void Set_Next_Or_Regular_State(EPlatform_Substate_Regular new_regular_state);
 
 	AsPlatform_State Platform_State;
 	bool Left_Key_Down, Right_Key_Down;
 
 	int Inner_Width;
 	int Rolling_Step;
+	int Last_Redraw_Timer_Tick;
 	double Speed;
-
 	double Glue_Spot_Height_Ratio;
 	double Expanding_Platform_Width;
 
